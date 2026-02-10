0e2cae7b05fe3e0a12fe29fc37379aeabb5acd0b
Rajdeep Kanzariya <rajdeepkanzariya054@gmail.com>
2026-02-10 16:11:51 +0530

    added file3

--- app.js
+++ app.js
@@ -8,8 +8,8 @@ const app = express();
 const PORT = 3000;
 
 app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
