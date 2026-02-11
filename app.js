f109f1b78351a7727729d6ed6a1726292c356ffe
Rajdeep Kanzariya <rajdeepkanzariya054@gmail.com>
2026-02-11 13:16:38 +0530

    added file3

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const path = require('path');
 const con = require('./config/db'); // mysql2 promise pool
 const app = express();
 const PORT = 3000;
+const authRoutes = require('./routes/auth.routes');
 
 app.use(cors());
 app.use(express.json());
@@ -13,10 +14,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-  res.render('signup');
-});
-
+app.use('/', authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
