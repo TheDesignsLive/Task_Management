82b36c360b1555c715d37dd62aa2025c4be821be
Rajdeep Kanzariya <rajdeepkanzariya054@gmail.com>
2026-02-13 13:23:41 +0530

    added file3

---
+++ routers/add-role.routes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const con = require('../config/db');
+
+// ADD CATEGORY (ROLE)
+router.post('/add-role', async (req, res) => {
+  try {
+    const adminId = req.session.adminId;
+    const { role_name, control_type } = req.body;
+    const can_manage_members = req.body.can_manage_members ? 1 : 0;
+
+    await con.execute(
+      `INSERT INTO roles (admin_id, role_name, control_type, can_manage_members)
+       VALUES (?, ?, ?, ?)`,
+      [adminId, role_name, control_type, can_manage_members]
+    );
+
+    res.redirect('/view_member'); // back to pages
+
+  } catch (err) {
+    console.error(err);
+    res.send("Error creating role");
+  }
+});
+
+module.exports = router;
