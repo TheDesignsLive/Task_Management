7aa7e5246b5a1a084a5b1f084ef7cbec71d85933
Jay <jaykatakiyajadav69@gmail.com>
2026-02-14 12:13:47 +0530

    added file3

---
+++ routers/logout.routes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// Logout Route
+router.get('/logout', (req, res) => {
+    req.session.destroy((err) => {
+        if (err) {
+            console.log("Logout Error:", err);
+            return res.send("Error logging out");
+        }
+
+        res.clearCookie('connect.sid'); // destroy session cookie
+        res.redirect('/'); // go to signup page
+    });
+});
+
+module.exports = router;
