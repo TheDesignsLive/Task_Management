65aa1050cbf47e2e203142801e0f699e74f7d958
Jay <jaykatakiyajadav69@gmail.com>
2026-02-18 16:55:26 +0530

    added file3

--- routers/sent-mail.routes.js
+++ routers/sent-mail.routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const nodemailer = require('nodemailer');
 
 router.post("/forgot-password/send-otp", async (req, res) => {
-    const { contact, otp } = req.body;
+    const { contact, otp,sent_for } = req.body;
 
     // Create transporter (use your email credentials)
     let transporter = nodemailer.createTransport({
@@ -13,14 +13,30 @@ router.post("/forgot-password/send-otp", async (req, res) => {
             pass: "yjoy emfi hkuq ncuj" // Use App Password for Gmail
         }
     });
+    let mailOptions=null;
 
-    // Email options
-    let mailOptions = {
+    // if sent_for is "forget_password", use this subject and text
+
+    if(sent_for=="forget_password"){
+
+    mailOptions = {
         from: 'jay13981398@gmail.com',
         to: contact,
         subject: "Your OTP for Password Reset",
         text: `Your OTP is ${otp}`
     };
+}
+
+    // if sent_for is "signup", use this subject and text make your own way for signup send message
+        if(sent_for=="signup"){
+
+    mailOptions = {
+        from: 'jay13981398@gmail.com',
+        to: contact,
+        subject: "Your OTP for Signup Verification",
+        text: `Your OTP is ${otp}`
+    };
+}
 
     try {
         await transporter.sendMail(mailOptions);
