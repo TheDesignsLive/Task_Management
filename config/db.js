70c07ae88333cfc5fc33d40185a3ccf4fdffbd21
Rajdeep Kanzariya <rajdeepkanzariya054@gmail.com>
2026-02-21 19:56:52 +0530

    A B C D

--- config/db.js
+++ config/db.js
@@ -3,9 +3,9 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: "localhost",
-  user: "root",
-  password: "",
-  database: "task_management",
+  user: "dilip",
+  password: "Dilip@8133",
+  database: "tmsDB",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
