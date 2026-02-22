5595ad0489674568b8a5859dcaacef31fdd01d8a
Rajdeep Kanzariya <rajdeepkanzariya054@gmail.com>
2026-02-22 11:45:04 +0530

    A B C D

--- config/db.js
+++ config/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+ host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "u213405511_dilip",
+  password: process.env.DB_PASS || "Dilip@8133",
+  database: process.env.DB_NAME || "u213405511_tmsDB",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
