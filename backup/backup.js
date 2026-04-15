c201ac49ac6a83a60f8f96f522da9eb6ba89d227
Jay Katakiyajadav <jaykatakiyajadav69@gmail.com>
2026-04-15 14:23:16 +0530

    Update backup.js

--- backup/backup.js
+++ backup/backup.js
@@ -5,12 +5,17 @@ const path = require('path');
 const con = require('../config/db');
 
 // const serviceAccountPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
-const serviceAccountPath = '/home/u213405511/domains/tms.thedesigns.live/nodejs/service-account.json';
+const serviceAccountPath = path.join(__dirname, '../service-account.json');
 
 console.log("📌 Service Account Path:", serviceAccountPath);
 
+if (!fs.existsSync(serviceAccountPath)) {
+    console.error("❌ service-account.json NOT FOUND at:", serviceAccountPath);
+    process.exit(1);
+}
+
 const auth = new google.auth.GoogleAuth({
-    keyFile: serviceAccountPath || path.join(__dirname, '../service-account.json'),
+    keyFile: serviceAccountPath,
     scopes: ['https://www.googleapis.com/auth/drive']
 });
 
@@ -48,10 +53,16 @@ async function backupDatabase() {
 
         await mysqldump({
             connection: {
-                host: process.env.DB_HOST,
-                user: process.env.DB_USER,
-                password: process.env.DB_PASS,
-                database: process.env.DB_NAME,
+                // host: process.env.DB_HOST,
+                // user: process.env.DB_USER,
+                // password: process.env.DB_PASS,
+                // database: process.env.DB_NAME,
+
+                host: process.env.DB_HOST || "srv832.hstgr.io",
+                user: process.env.DB_USER|| "u213405511_dilip",
+                password: process.env.DB_PASS || "Dilip@8133",
+                database: process.env.DB_NAME || "u213405511_tmsDB"
+
             },
             dumpToFile: filePath,
         });
