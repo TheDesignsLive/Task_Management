06ab019370340cfc50f0c17bbf94030f43b5dd10
Jay Katakiyajadav <jaykatakiyajadav69@gmail.com>
2026-04-15 12:38:39 +0530

    Update app.js

--- app.js
+++ app.js
@@ -262,5 +262,5 @@ app.use("/masterpage",panel)
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => { // Changed app.listen to server.listen
     console.log("Server running on port " + PORT);
-      scheduleBackup(12, 23, "PM");
+      scheduleBackup(12, 40, "PM");
 });
