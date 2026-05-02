dff4251118511cb608cfd1f323f8327bc1d1711a
Rajdeep Kanzariya <rajdeepkanzariya054@gmail.com>
2026-05-02 10:01:48 +0530

    auto push

--- routers/profile.routes.js
+++ routers/profile.routes.js
@@ -9,7 +9,6 @@ const storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, 'public/images'),
     filename: (req, file, cb) => cb(null, Date.now() + "_" + file.originalname)
 });
-
 const upload = multer({ 
     storage,
     limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
